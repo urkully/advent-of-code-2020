a2a2bb63f46bb83497fd80d7f2512e9dfd7e3a5f
urkully <63817975+urkully@users.noreply.github.com>
2020-12-06 19:46:47 +0000

    Day 2 start

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,33 +1,60 @@
+import math
+
 def sumOfTwoNumbers(first, second):
     return first + second
 
 def sumOfTwoNumbersEquals(first, second, sum):
     return (first + second) == sum
 
+def productOfFactors(factors):
+    return math.prod(factors)
+
 def inputFileAsListofIntegers(filename):
     with open(filename) as file:
         lines = file.readlines()
         return [int(line) for line in lines]
 
-def doAnyTwoIntegersInAListSumToDesiredValue(integers, desiredSum):
-    for firstInteger in integers:
-        for secondInteger in integers:
-            if sumOfTwoNumbersEquals(firstInteger, secondInteger, desiredSum):
-                print("**")
-                print(firstInteger)
-                print(secondInteger)
-                return True
+def whichTwoIntegersInAListSumToDesiredValue(integers, desiredSum):
+    # Cannot sum with less than two integers
+    if len(integers) < 2:
+        return []
+
+    # Don't want to sum elements with themselves
+    firstInteger = integers.pop()
+
+    for secondInteger in integers:
+        if sumOfTwoNumbersEquals(firstInteger, secondInteger, desiredSum):
+           return [firstInteger, secondInteger]
+
+    return whichTwoIntegersInAListSumToDesiredValue(integers, desiredSum)
+
+def whichThreeIntegersInAListSumToDesiredValue(integers, desiredSum):
+    # Cannot sum with less than two integers
+    if len(integers) < 3:
+        return []
+
+    # Don't want to sum elements with themselves
+    firstInteger = integers.pop()
+    for secondInteger in integers:
+        if sumOfTwoNumbersEquals(firstInteger, secondInteger, desiredSum):
+           return [firstInteger, secondInteger]
+
+    return whichThreeIntegersInAListSumToDesiredValue(integers, desiredSum)
+
+
 
 def main():
-    with open('Day1Input.txt') as file:
-        lines = file.readlines()
-        for first in lines:
-            for second in lines:
-                for third in lines:
-                    sum = (int(first) + int(second) + int(third))
-                    if sum == 2020:
-                        multiplied = (int(first) * int(second) * int(third))
-                        print("multiplied is " + str(multiplied))
+
+    # Exercise 1
+    listOfIntegers = inputFileAsListofIntegers('Day1Input.txt')
+    twoNumbers = whichTwoIntegersInAListSumToDesiredValue(listOfIntegers, 2020)
+    answerForExerciseOne = productOfFactors(twoNumbers)
+    print("Answer for exercise 1 is " + str(answerForExerciseOne))
+
+    #Exercise 2
+    threeNumbers = whichThreeIntegersInAListSumToDesiredValue(listOfIntegers, 2020)
+    answerForExerciseTwo = productOfFactors(threeNumbers)
+    print("Answer for exercise 2 is " + str(answerForExerciseTwo))
 
 if __name__ == "__main__":
     main()
--- Day1/Day1Tests.py
+++ Day1/Day1Tests.py
@@ -4,7 +4,9 @@ import unittest
 from Day1 import sumOfTwoNumbers
 from Day1 import sumOfTwoNumbersEquals
 from Day1 import inputFileAsListofIntegers
-from Day1 import doAnyTwoIntegersInAListSumToDesiredValue
+from Day1 import whichTwoIntegersInAListSumToDesiredValue
+from Day1 import whichThreeIntegersInAListSumToDesiredValue
+from Day1 import productOfFactors
 
 class SummingNumbers(unittest.TestCase):
     def testSumOfTwoNumbersIsCorrect(self):
@@ -28,13 +30,25 @@ class ReadingInput(unittest.TestCase):
         self.assertEqual(lines[1], 2)
         self.assertEqual(lines[2], 3)
 
+class FindingSumsOfTwoInList(unittest.TestCase):
+    def testCannotHaveLessThanTwoIntegers(self):
+        self.assertCountEqual(whichTwoIntegersInAListSumToDesiredValue([1], 2), [])
+
     def testCanFindDesiredSumOfTwoIntegers(self):
-        self.assertTrue(doAnyTwoIntegersInAListSumToDesiredValue([1, 2, 3], 5))
+        self.assertCountEqual(whichTwoIntegersInAListSumToDesiredValue([1, 2, 3], 5), [2, 3])
 
     def testCanFailToFindDesiredSumOfTwoIntegers(self):
-        self.assertTrue(doAnyTwoIntegersInAListSumToDesiredValue([1, 2, 3], 2))
+        self.assertCountEqual(whichTwoIntegersInAListSumToDesiredValue([1, 2, 3], 7), [])
+
+class FindingSumsOfThreeInList(unittest.TestCase):
+    def testCannotHaveLessThanThreeIntegers(self):
+        self.assertCountEqual(whichThreeIntegersInAListSumToDesiredValue([1, 2], 2), [])
 
 
+
+class FindingProductOfFactors(unittest.TestCase):
+    def testCorrectProductWithTwoFactors(self):
+        self.assertEqual(productOfFactors([2, 3]), 6)
 
 if __name__ == '__main__':
     unittest.main()
